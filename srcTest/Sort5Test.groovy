b60441e734c54db8fc25cc3eac980f3e7c533dfa
Yu Kobayashi <yukoba@accelart.jp>
2014-01-25 14:04:31 +0900

    Sort5Test整理

--- srcTest/Sort5Test.groovy
+++ srcTest/Sort5Test.groovy
@@ -1,22 +1,26 @@
-
-
 import org.junit.Test
+
+import static Sort5.*
 import static org.junit.Assert.*
 
 class Sort5Test {
-	@Test void test() {
+    @Test
+    void testNodeBuilder() {
         def nb = new NodeBuilder()
 
-        def n1 = nb.ab(a:1) { cd() }
+        def n1 = nb.ab(a: 1) { cd() }
         println "n1 = $n1"
 
-        //def n2 = nb.ab { cd=1 }
+//        def n2 = nb.ab { cd=1 }
 //        def n2 = {foo a=1 b="abc"} as Node
-//        println n2
+//        println "n2 = $n2"
+    }
 
-        def e1 = "A.tail.head == A.swap.head"
-        def e3 = "head(tail(A)) == head(swap(A))"
-        def e2 = "f( if(a, b, c) ) == if(a, f(b), f(c))"
+    @Test
+    void test() {
+//        def e1 = "A.tail.head == A.swap.head"
+//        def e3 = "head(tail(A)) == head(swap(A))"
+//        def e2 = "f( if(a, b, c) ) == if(a, f(b), f(c))"
 //        def e4 = "if(TRUE, a, b) == a"
         def e4 = "if(TRUE, a(c,d), b) == a"
 
@@ -27,57 +31,5 @@ class Sort5Test {
 
         println replaceNode(e4_3, ["a", "c", "d"], "e");
         println replaceNode(["a", ["a", ["c", "d"]], ["a", ["c", "d"]]], ["a", ["c", "d"]], "e");
-    }
-
-    static String removeSpace(String s) { s.replaceAll(' ', '') }
-
-    static def parseFunc(String s) {
-        List list = []
-        int start = s.indexOf('(')
-        if (start == -1) return s
-        int end = s.lastIndexOf(')')
-        list.add(s[0..(start-1)])
-
-        int bracketCount = 0
-        StringBuilder sb = new StringBuilder()
-        for (int i = start + 1; i < end; i++) {
-            switch (s[i]) {
-                case "(": bracketCount++; sb.append(s[i]); break;
-                case ")": bracketCount--; sb.append(s[i]); break;
-                case ",":
-                    if (bracketCount == 0) {
-                        list.add(parseFunc(sb.toString()))
-                        sb = new StringBuilder()
-                    } else {
-                        sb.append(s[i])
-                    }
-                    break
-                default:
-                    sb.append(s[i])
-                    break
-            }
-        }
-        assert bracketCount == 0
-        if (sb.size() > 0)
-            list.add(parseFunc(sb.toString()))
-        return list
-    }
-
-    static def replaceNode(expr, from, to, List replaced = [false]) {
-        if (replaced[0])
-            return expr
-        if (expr == from) {
-            replaced[0] = true
-            return to
-        }
-        switch (expr) {
-            case String:
-                return expr
-            case List:
-                int idx = 0
-                return expr.collect { idx++ == 0 ? it : replaceNode(it, from, to, replaced) }
-            default:
-                return null
-        }
     }
 }
